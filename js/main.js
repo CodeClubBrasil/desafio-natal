a72e1091c6fc04387bd96b8325ede71eee79896f
Felipe Fernandes <ripcorp@gmail.com>
2014-11-16 17:27:27 -0200

    WIP (2)
    @todo Ainda falta:
     - validação do formulário de inscrição
     - revisão / redação do sobre o projeto
     - redação do termos de uso
     - desenvolvimento da mecânica de CRUD para os projetos inscritos.
     - revisão geral.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
+$(document).ready(function() {
 
+	$('.btn-link').click(function() {
+
+		console.log("CLICK");
+
+		var areaId = $(this).attr('href');
+		
+		$('body').scrollTo( areaId, 1000, { offset: 0 } );//add or deduct from the final position	
+	});    
+
+
+
+	$('.open-popup-link').magnificPopup({
+	  type:'inline',
+	  
+	  // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.	  
+	  midClick: true, 
+  	  
+	  // Delay in milliseconds before popup is removed
+  	  removalDelay: 300, 
+
+	  // Class that is added to popup wrapper and background
+	  // make it unique to apply your CSS animations just to this exact popup
+	  mainClass: 'mfp-fade'
+	});
+
+});
